ac750373afd959d537185e8722ffaf34f1faf4fa
Bryce Jech <bryce@brycejech.com>
2022-09-17 10:02:34 -0500

    tweet formatting

--- go-twitter/twitter/twitter.go
+++ go-twitter/twitter/twitter.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -37,6 +38,28 @@ type GetTweets struct {
 }
 
 const BASE_URL = "https://api.twitter.com"
+
+func getStringOfLen(str string, len int) string {
+	var final string
+	for i := 0; i < len; i++ {
+		final += str
+	}
+	return final
+}
+
+func getLongestString(strings []string) (longest string, length int) {
+	longestStr := ""
+	longestLen := 0
+
+	for _, str := range strings {
+		if len(str) > longestLen {
+			longestLen = len(str)
+			longestStr = str
+		}
+	}
+
+	return longestStr, longestLen
+}
 
 func getBasicAuthToken() string {
 	var apiKey string = os.Getenv("API_KEY")
@@ -131,9 +154,9 @@ func GetTwitterUserByHandle(handle string) TwitterUser {
 	return userData.Data
 }
 
-func GetTweetsByUserId(userId string) []Tweet {
+func GetTweetsByUserId(userId string, max int) []Tweet {
 	method := "GET"
-	url := fmt.Sprintf("%v/2/users/%v/tweets?max_results=5&tweet.fields=id,text,created_at", BASE_URL, userId)
+	url := fmt.Sprintf("%v/2/users/%v/tweets?max_results=%v&tweet.fields=id,text,created_at", BASE_URL, userId, max)
 
 	userTweets := doRequest[GetTweets](method, url)
 
@@ -141,24 +164,65 @@ func GetTweetsByUserId(userId string) []Tweet {
 }
 
 func FormatTweet(tweet Tweet, handle string) string {
-	const length = 100
+	const MAX_LEN = 65
 
-	var strings []string
+	var final []string
 
+	var titleLine []string
 	createdAt, _ := time.Parse(time.RFC3339, tweet.CreatedAt)
 	formattedTime := createdAt.Format("Jan 02 2006")
-	title := fmt.Sprintf("On %v, %v tweeted:\n\n", formattedTime, handle)
-	strings = append(strings, title)
+	title := fmt.Sprintf("On %v, %v tweeted:", formattedTime, handle)
+	titleLine = append(titleLine, title)
 
-	var tmpStr string
-	for _, char := range tweet.Text {
-		str := string(char)
-		if str == "\n" {
-			strings = append(strings, tmpStr)
-			tmpStr = ""
+	lines := []string{strings.Join(titleLine, " ")}
+
+	// split the tweet text into user entered "\n" chars
+	tmpLines := strings.Split(tweet.Text, "\n")
+
+	for _, line := range tmpLines {
+		// if line length under threshold, let through
+		if len(line) < MAX_LEN {
+			lines = append(lines, line)
+			continue
 		}
-		tmpStr += str
-		// if()
+
+		// Otherwise, we need to shorten it to just under 60
+		tmpLine := ""
+		words := strings.Split(line, " ")
+
+		for i, word := range words {
+			// if adding the current word passes threshold, reset
+			if len(tmpLine+" "+word) > MAX_LEN {
+				lines = append(lines, tmpLine)
+				tmpLine = word
+
+			} else {
+				tmpLine = fmt.Sprintf("%v %v", tmpLine, word)
+			}
+
+			if i == len(words)-1 {
+				lines = append(lines, tmpLine)
+			}
+		}
 	}
-	return ""
+
+	_, longest := getLongestString(lines)
+
+	FINAL_LEN := MAX_LEN
+	if longest < MAX_LEN {
+		FINAL_LEN = longest
+	}
+
+	sep := fmt.Sprintf("# %v #", getStringOfLen("-", FINAL_LEN))
+	for i, line := range lines {
+		if i == 0 {
+			final = append(final, line /* title */, sep)
+			continue
+		}
+		tweetLine := fmt.Sprintf("| %-*v |", FINAL_LEN, strings.Trim(line, " "))
+		final = append(final, tweetLine)
+	}
+	final = append(final, sep)
+
+	return strings.Join(final, "\n")
 }
